c952305c31903acd1c32e3bdbc7d714a89b444fb
steven3ia3 <steven.arce.arebalo@gmail.com>
2023-12-13 00:06:58 +0100

    terminado el ejercicio dungeon crawl

--- DungeonCrawl/MapGrid.py
+++ DungeonCrawl/MapGrid.py
@@ -6,32 +6,82 @@ class MapGrid:
         self.width = width
         self.height = height
         self.walls = []
-        self.map = [[]]
+
+    def create_grid(self):
+        self.map = [[] for y in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
+                self.map[y].append(". ")
+
+        # posicion inicial del jugador
+        self.player_x = 0
+        self.player_y = 0
+        # posición inicial del jugador
+        self.map[self.player_y][self.player_x] = '$ '
+
+        # entrada en la esquina superior izquierda
+        self.start_x, self.start_y = 0, 0
+        # salida en la esquina inferior derecha
+        self.end_x, self.end_y = self.width - 1, self.height - 1
+        # representa la salida
+        self.map[self.end_y][self.end_x] = "> "
 
     def draw_grid(self):
-        self.map = [['. ' for x in range(self.width)] for y in range(self.height)]
-    """
-    def draw_grid(self):
-        self.map = [[0 for x in range(self.width)] for y in range(self.height)]
-        for x in range(self.height):
-            for y in range(self.width):       
-                map[x][y] = ". "           
-        #print(map)
-    """
+        for y in range(self.height):
+            for x in range(self.width):
+                print(self.map[y][x], end=" ")
+            print()
+
     def get_walls(self, pct=0.15):
         num_walls = round(self.width * self.height * pct)
+        # tupla de dos valores
+        coordinates = list()
+        # obtener todas las coordenadas posibles excepto entrada y salida
+        for y in range(self.height):
+            for x in range(self.width):
+                if (x, y) != (self.start_x, self.start_y) and (x, y) != (self.end_x, self.end_y):
+                    coordinates.append((x, y))
+        # shuffle mezcla aleatoriamente la tupla
+        random.shuffle(coordinates)
+        # coger las primeras num_walls(67) elementos
+        chosen = coordinates[:num_walls]
 
-        chosen = list()
-
-        for i in range(num_walls):
-            y = random.randint(0, self.height - 1)
-            x = random.randint(0, self.width - 1)
-
-            
-
-            chosen.append((x, y))
-            
+        for x, y in chosen:
             self.map[y][x] = "# "
             
-        print(self.map)
+    
+    def move_player(self, direction):
+       
+        # calcular la nueva posicion del jugador
+        new_x, new_y = self.player_x, self.player_y
+        if direction == 'r' and self.player_x < self.width - 1:
+            # derecha
+            new_x += 1
+        elif direction == 'l' and self.player_x > 0:
+            # izquierda
+            new_x -= 1
+        elif direction == 'u' and self.player_y > 0:
+            # arriba
+            new_y -= 1
+        elif direction == 'd' and self.player_y < self.height - 1:
+            # abajo
+            new_y += 1
 
+        # verificar si la nueva posición está dentro de la lista de muros
+        if self.map[new_y][new_x] != "# ":
+            # limpiar la posición actual del jugador
+            self.map[self.player_y][self.player_x] = ". "
+            # actualizar la posición del jugador en el mapa
+            self.player_x, self.player_y = new_x, new_y
+            # volver a actulizar el simbolo del jugador
+            self.map[self.player_y][self.player_x] = "$ "
+        else:
+            # la nueva posición es un muro, el jugador no se mueve
+            print("There is a wall!")
+        
+
+        """
+        # verificar si el jugador ha alcanzado la salida
+        if map.player_x == map.end_x and map.player_y == map.end_y:
+            print("You made it to the end!")
+        """
--- DungeonCrawl/dungeonCrawl.py
+++ DungeonCrawl/dungeonCrawl.py
@@ -2,9 +2,14 @@ from MapGrid import MapGrid
 
 map = MapGrid()
 
-map.draw_grid()
+map.create_grid()
 map.get_walls()
 
-
-
-
+while (map.player_x, map.player_y) != (map.end_x, map.end_y):
+    # dibujar el mapa
+    map.draw_grid()
+    # recoger el movimiento del jugador
+    movement = input("Which way? (r, l, u, d): ")
+    # mover al jugador
+    map.move_player(movement)
+print("You made it to the end!")
